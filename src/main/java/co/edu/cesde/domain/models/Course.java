924c7b34eaf7806e5d006dc7c5de4e3ebc1a8f15
SantiagoArenas <arenassanty075@gmail.com>
2026-07-30 10:16:24 -0500

    feat: crear entidad Course

---
+++ src/main/java/co/edu/cesde/domain/models/Course.java
@@ -0,0 +1,60 @@
+package co.edu.cesde.domain.models;
+
+public class Course {
+    Long id;
+    String code;
+    String name;
+    String description;
+    Integer maxCapacity;
+
+    public Course(Long id, String code, String name, String description, Integer maxCapacity) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.maxCapacity = maxCapacity;
+    }
+
+    public Course() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(Integer maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+}
