55e7c419c760d223c389f1b146cb6e86d3a482de
SantiagoArenas <arenassanty075@gmail.com>
2026-07-30 11:56:34 -0500

    Refactor menu principal y menus del sistema de cursos

--- src/main/java/co/edu/cesde/presentation/Main.java
+++ src/main/java/co/edu/cesde/presentation/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        mostrarMenuPrincipal();
+
+
+    }
+
+    private static void mostrarMenuPrincipal() {
+
         System.out.println("=================================");
         System.out.println(" SISTEMA DE GESTIÓN DE CURSOS");
         System.out.println("=================================");
