6ba0256d8129382e8595415f749e0d614fef4790
SantiagoArenas <arenassanty075@gmail.com>
2026-07-30 11:34:55 -0500

    feat: agregar menú principal

---
+++ src/main/java/co/edu/cesde/presentation/Main.java
@@ -0,0 +1,15 @@
+package co.edu.cesde.presentation;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" SISTEMA DE GESTIÓN DE CURSOS");
+        System.out.println("=================================");
+        System.out.println("1. Students");
+        System.out.println("2. Courses");
+        System.out.println("3. Enrollments");
+        System.out.println("0. Exit");
+    }
+}
